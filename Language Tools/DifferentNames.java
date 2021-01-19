02964c47f977a03fefb5171979429fd6f38bd143
Lisa verma <68117174+vermalisa11@users.noreply.github.com>
2021-01-19 21:15:21 +0530

    Update and rename Different Names to DifferentNames.java

--- Language Tools/DifferentNames.java
+++ Language Tools/DifferentNames.java
@@ -1,4 +1,4 @@
-In Little Flowers Public School, there are many students with same first names. You are given a task to find the students with same names. You will be given a string comprising of all the names of students and you have to tell the name and count of those students having same. If all the names are unique, print -1 instead.
+/*In Little Flowers Public School, there are many students with same first names. You are given a task to find the students with same names. You will be given a string comprising of all the names of students and you have to tell the name and count of those students having same. If all the names are unique, print -1 instead.
 Note: We don't have to mention names whose frequency is 1.
 Input Format:
 The only line of input will have a string ‘str’ with space separated first names of students.
@@ -17,8 +17,9 @@ Sample Input 2:
 Abhishek Harshit Ayush Iti
 Sample Output:
 -1
+*/
 
-Solution---------------------------------------------------------
+//Solution---------------------------------------------------------
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
