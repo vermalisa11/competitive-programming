46e19cd48aa49598254cbde8012d43ea4df20cdc
Lisa verma <68117174+vermalisa11@users.noreply.github.com>
2021-04-18 19:09:55 +0530

    Create distinctSunsequences.java

---
+++ dynamic-programming-2/distinctSunsequences.java
@@ -0,0 +1,62 @@
+/*
+Distinct Subsequences
+Send Feedback
+Given a string, count the number of distinct subsequences of it ( including empty subsequence ). For the uninformed, A subsequence of a string is a new string which is formed from the original string by deleting some of the characters without disturbing the relative positions of the remaining characters.
+For example, "AGH" is a subsequence of "ABCDEFGH" while "AHG" is not.
+Input
+First line of input contains an integer T which is equal to the number of test cases. You are required to process all test cases. Each of next T lines contains a string s.
+Output
+Output consists of T lines. Ith line in the output corresponds to the number of distinct subsequences of ith input string. Since, this number could be very large, you need to output ans%1000000007 where ans is the number of distinct subsequences. 
+Constraints and Limits
+T ≤ 100, length(S) ≤ 100000
+
+All input strings shall contain only uppercase letters.
+Sample Input
+3
+AAA
+ABCDEFG
+CODECRAFT
+Sample Output
+4
+128
+496
+*/
+import java.util.*;
+import java.io.*;
+public class Main {
+	static int mod = 1000000007;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		// Write your code here
+        int t = Integer.parseInt(br.readLine());
+        while(t-->0){
+            String str = br.readLine();
+		
+            int[] last = new int[26];
+            Arrays.fill(last, -1);
+
+            int n = str.length();
+
+
+            int[] dp = new int[n + 1];
+
+            dp[0] = 1;
+
+            for (int i = 1; i <= n; i++) {
+                dp[i] = (2 * (dp[i - 1]%mod))%mod;
+                if(last[(int)str.charAt(i-1)-65]!=-1){
+						dp[i]=(dp[i]-dp[(int)last[str.charAt(i-1)-65]]+mod)%mod;
+				}
+				last[(int)str.charAt(i-1)-65]=i-1;
+
+//                 if (last[(int)str.charAt(i - 1)] != -1)
+//                     dp[i] = (dp[i]%mod - dp[last[(int)str.charAt(i - 1)]]%mod)%mod;
+
+//                 last[(int)str.charAt(i - 1)] = (i - 1);
+            }
+
+            System.out.println(dp[n]%mod);
+        }
+	}
+}
