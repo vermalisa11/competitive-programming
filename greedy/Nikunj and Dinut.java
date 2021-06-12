94cc02191fa1aa9db6e6f51a19dfe42cb264c60f
Prince Nandha <68117143+PrinceNandha@users.noreply.github.com>
2021-06-12 15:30:28 +0530

    Create Nikunj and Dinut.java

---
+++ greedy/Nikunj and Dinut.java
@@ -0,0 +1,38 @@
+/*Nikunj loves donuts, but he also likes to stay fit. He eats n donuts in one sitting, and each donut has a calorie count, ci. After eating a donut with k calories, he must walk at least 2^j x k(where j is the number donuts he has already eaten) miles to maintain his weight.
+Given the individual calorie counts for each of the n donuts, find and print a long integer denoting the minimum number of miles Nikunj must walk to maintain his weight. Note that he can eat the donuts in any order.
+Input
+The first line contains an integer, n, denoting the number of donuts. 
+The second line contains n space-separated integers describing the respective calorie counts of each donut I, i.e ci.
+Output
+Print a long integer denoting the minimum number of miles Nikunj must walk to maintain his weight.
+Constraints
+1 ≤ n ≤ 40
+1 ≤ ci ≤ 1000
+Sample Input
+3
+1 3 2
+Sample Output
+11*/
+
+import java.util.*;
+
+public class Main {
+
+	static Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
+		// Write your code here
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
+        Arrays.sort(arr);
+		int j = 0;
+        long ans = 0;
+        while(n-->0){
+            ans+= arr[n]*(long)Math.pow(2,j++);
+        }
+        System.out.println(ans);
+	}
+
+}
